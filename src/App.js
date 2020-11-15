6caf12f80264e823243007dda0b237c63c69f6db
Guo Kaitong <guokt@seagroup.com>
2020-11-15 18:25:25 +0800

    form and layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Form, Spin, Input, Button, Tag,
+} from 'antd';
+import { useState } from 'react';
+
+function TitleArea({ faceId }) {
+  return (
+    <div style={{ marginBottom: '50px', textAlign: 'center' }}>
+      <h1>Welcome</h1>
+      <p style={{ fontSize: '1rem' }}>
+        You have a new visiter with face ID:&nbsp;
+        <Tag color="blue">
+          {faceId}
+        </Tag>
+      </p>
+      <p style={{ fontSize: '1rem' }}>
+        You can fill in this form to grant him/her access or simply ignore
+      </p>
+    </div>
+  )
+}
+
+function InputArea() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = (values) => {
+    console.log(values)
+    setIsLoading(true)
+    // TODO: implement submit function to backend
+  }
+  return (
+    <Spin spinning={isLoading}>
+      <div
+        style={{
+          padding: '50px 30px 10px 30px',
+          border: '1px solid #40A9FF',
+          borderRadius: '8px',
+        }}
+      >
+        <Form
+          onFinish={onSubmit}
+          initialValues={{}}
+        >
+          <Form.Item
+            label="Phone"
+            name="phone"
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Name"
+            name="name"
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item style={{ textAlign: 'center' }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+          </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </Spin>
+  );
+}
 
 function App() {
+  const urlParam = window.location.search;
+  const urlParamContent = urlParam.slice(1) || '';
+  const urlParamKey = urlParamContent.split('=')[0];
+  const urlParamValue = urlParamContent.split('=')[1];
+  if (urlParamKey !== 'faceId' || !urlParamValue) {
+    alert('No faceId Specified!')
+    return null
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        paddingTop: '60px'
+      }}
+    >
+      <div
+        style={{ width: 500 }}
+      >
+        <TitleArea faceId={urlParamValue} />
+        <InputArea />
+      </div>
     </div>
   );
 }
